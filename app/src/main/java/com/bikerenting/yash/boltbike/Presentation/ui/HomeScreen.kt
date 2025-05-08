0eb8a75aebb8aee618bf7ca89d89049df3de1eda
Suyash <suyash.s@ventla.io>
2025-05-08 19:22:04 +0530

    Profile Screen
    Intialize

--- app/src/main/java/com/bikerenting/yash/boltbike/Presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/bikerenting/yash/boltbike/Presentation/ui/HomeScreen.kt
@@ -15,18 +15,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -53,7 +50,6 @@ import com.bikerenting.yash.boltbike.Presentation.TextPrimary
 import com.bikerenting.yash.boltbike.Presentation.TextSecondary
 import com.bikerenting.yash.boltbike.Presentation.viewmodel.MainListingViewModel
 import com.bikerenting.yash.boltbike.R
-import java.util.Objects
 
 @Composable
 fun HomeScreen() {
@@ -126,7 +122,7 @@ fun BikeCard(vehicle: Vehicle) {
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(text = vehicle.brand, fontSize = 20.sp, fontWeight = FontWeight.Bold)
-               // Text(text = vehicle.type, color = Color.Gray)
+                // Text(text = vehicle.type, color = Color.Gray)
                 Text(text = "Plate: ${vehicle.numberPlate}", color = Color.DarkGray)
                 Text(
                     text = "Parking: ${vehicle.currentLocation.parkingArea}",
@@ -161,7 +157,7 @@ fun BikeCard(vehicle: Vehicle) {
 }
 
 @Composable
-fun HeaderView(
+private fun HeaderView(
     context: Context,
     user_name: String,
     user_current_location: String,
--- app/src/main/java/com/bikerenting/yash/boltbike/Presentation/ui/ProfileScreen.kt
+++ app/src/main/java/com/bikerenting/yash/boltbike/Presentation/ui/ProfileScreen.kt
@@ -1,5 +1,40 @@
 package com.bikerenting.yash.boltbike.Presentation.ui
 
-class ProfileScreen {
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.bikerenting.yash.boltbike.Presentation.viewmodel.MainListingViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
+@Composable
+fun ProfileScreen() {
+    val profile_shared_data = remember { MainListingViewModel() }
+    val context = LocalContext.current
+    val current_user = FirebaseAuth.getInstance().currentUser
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background)
+    ){
+        Column {
+            HeaderView(
+                context,
+                current_user
+            )
+        }
+    }
 }
+
+@Composable
+private fun HeaderView(context: Context, currentUser: FirebaseUser?) {
+    
+}
+
