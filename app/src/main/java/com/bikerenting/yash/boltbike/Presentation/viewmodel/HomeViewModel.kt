c896a033a8f0d808645e3a0c7b37ba4574e722ab
Suyash Shukla <shuklasuyash52@gmail.com>
2025-06-14 18:49:21 +0530

    Update HomeViewModel.kt

--- app/src/main/java/com/bikerenting/yash/boltbike/Presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/bikerenting/yash/boltbike/Presentation/viewmodel/HomeViewModel.kt
@@ -3,21 +3,42 @@ package com.bikerenting.yash.boltbike.Presentation.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bikerenting.yash.boltbike.Core.MyApp
+import com.bikerenting.yash.boltbike.Data.Local.DAO
 import com.bikerenting.yash.boltbike.Domain.Model.Vehicle
 import com.bikerenting.yash.boltbike.Data.Remote.ApiClient
 import com.bikerenting.yash.boltbike.Domain.Model.User
-import com.bikerenting.yash.boltbike.Presentation.ui.UserProfile
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    val _bikes = MutableStateFlow<List<Vehicle>>(emptyList())
+class HomeViewModel(
+    private val userDao: DAO
+) : ViewModel() {
+
+    private val _bikes = MutableStateFlow<List<Vehicle>>(emptyList())
     val bikes: StateFlow<List<Vehicle>> = _bikes
-    val _userData = MutableStateFlow<User>(User())
+
+    private val _userData = MutableStateFlow(User())
     val userData: StateFlow<User> = _userData
+
+    fun getUserDetail() {
+        viewModelScope.launch {
+            try {
+                userDao.getUser().collect { userEntity ->
+                    _userData.value = User(
+                        uid = userEntity.uid,
+                        name = userEntity.name,
+                        phoneNumber = userEntity.phoneNumber,
+                        email = userEntity.email
+                    )
+                }
+            } catch (e: Exception) {
+                Log.e("UserDetail", "Exception: ${e.localizedMessage}")
+            }
+        }
+    }
+
     fun getBikeList() {
         viewModelScope.launch {
             try {
