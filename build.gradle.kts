7289e26bbc93c8367454073c1a217cb7fcc2c75a
Suyash Shukla <shuklasuyash52@gmail.com>
2025-06-14 18:46:49 +0530

    Added Koin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+
 }
